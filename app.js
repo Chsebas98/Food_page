a116fc5efe975f8a6113d46d20728824f9b288be
Chsebas98 <chsebas98@gmail.com>
2022-01-22 20:21:09 -0500

    finish

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const express = require("express");
 require("dotenv").config();
 const hbs = require("hbs");
+const platos = require("./models/platos");
+
 //app
 const app = express();
 
 //middlewares
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "hbs");
-hbs.registerPartials(__dirname + "/views/partials");
+hbs.registerPartials(__dirname + "/views/components");
 
 //route
 app.get("/", (req, res) => {
-  res.render("main");
+	res.render("main", platos);
 });
 //port
 const port = process.env.PORT || 3000;
 //server
 const server = app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+	console.log(`Servidor corriendo en http://localhost:${port}`);
 });
